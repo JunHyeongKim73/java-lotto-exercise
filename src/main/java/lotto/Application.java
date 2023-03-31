922430e042b0ef777bfe3bdd171215ede9134113
JunHyeongKim73 <rla5764v@gmail.com>
2023-03-31 23:52:41 +0900

    feat: 로또 애플리케이션 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import controller.IOController;
+import domain.Lotto;
+import java.util.List;
+import service.LottoService;
+import service.LottoServiceImpl;
+import service.NumbersGenertor;
+import service.RandomNumbersGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final NumbersGenertor numbersGenertor = new RandomNumbersGenerator();
+        final LottoService lottoService = new LottoServiceImpl(numbersGenertor);
+        final IOController ioController = new IOController(lottoService);
+
+        System.out.println("구입금액을 입력해 주세요.");
+        final String purchaseInput = Console.readLine();
+        final List<Lotto> purchaseLottos = ioController.buyLotto(purchaseInput);
+
+        System.out.println("당첨번호를 입력해 주세요.");
+        final String winningInput = Console.readLine();
+        final Lotto winningLotto = ioController.getWinningLotto(winningInput);
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        final String bonusInput = Console.readLine();
+
+        ioController.printResult(purchaseLottos, winningLotto, bonusInput);
     }
 }
